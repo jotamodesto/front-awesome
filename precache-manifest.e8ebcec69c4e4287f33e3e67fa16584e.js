c6c5cdbf3a76f84e7a43c664777711a93494c431
Johnatan Modesto <johnatan.modesto@gmail.com>
2020-08-24 01:01:39 -0300

    Updates

---
+++ precache-manifest.e8ebcec69c4e4287f33e3e67fa16584e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fccc752111e88935c344cbe3c80184a",
+    "url": "/front-awesome/index.html"
+  },
+  {
+    "revision": "e0153c67dc84dc0ed8db",
+    "url": "/front-awesome/static/css/2.d22cd3da.chunk.css"
+  },
+  {
+    "revision": "e0153c67dc84dc0ed8db",
+    "url": "/front-awesome/static/js/2.dd109f7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/front-awesome/static/js/2.dd109f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4228ba3c59588e964c2",
+    "url": "/front-awesome/static/js/main.0323d1b1.chunk.js"
+  },
+  {
+    "revision": "a4484ec9040a613085ab",
+    "url": "/front-awesome/static/js/runtime-main.fdc146e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/front-awesome/static/media/logo.5d5d9eef.svg"
+  }
+]);
